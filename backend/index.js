d572725c3dd45a2e8bf615c8b2eab084b4a58286
pramathvb12 <106541314+pramathvb12@users.noreply.github.com>
2023-09-09 23:00:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ var cors = require("cors"); // importing cros
 
 connectMongo(); // connecting to mongo
 app.use(cors({
-      origin:["https://i-note-book-cloud-main.vercel.app"],
+      origin:'https://i-note-book-cloud-main.vercel.app',
       methods:["POST","GET","PUT","DELETE"],
       credentials:true
 })); // Cross-Origin Resource Sharing is a mechanism by which a front-end client can make requests for resources to an external back-end server.
